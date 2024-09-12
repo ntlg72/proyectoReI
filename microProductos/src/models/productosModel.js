c57051d35dc312825d0b40c97b819d0cc8aaa58e
ntlg72 <nt.lg2@hotmail.com>
2024-09-12 10:28:40 -0500

    Código de index, productosController y productoModel. Falta cambiar la BD y tablas.
    Co-authored-by: Liseth Esmeralda Erazo <memerazo@users.noreply.github.com>
    Co-authored-by: MICHEL DAHIANA BURGOS SANTOS <michel.burgos@uao.edu.co>

---
+++ microProductos/src/models/productosModel.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql2/promise');
+
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     port:'3307',
+//     database: 'productosDB'
+// });
+
+
+async function traerProductos() {
+    const result = await connection.query('SELECT * FROM productos');
+    return result[0];
+}
+
+
+async function traerProducto(id) {
+    const result = await connection.query('SELECT * FROM productos WHERE id = ?', id);
+    return result[0];
+}
+
+
+async function actualizarProducto(id, inventario) {
+    const result = await connection.query('UPDATE productos SET inventario = ? WHERE id = ?', [inventario, id]);
+    return result;
+}
+
+
+async function crearProducto(nombre, precio, inventario) {
+    const result = await connection.query('INSERT INTO productos VALUES(null,?,?,?,?)', [nombre,categoria, precio, inventario]);
+    return result;
+}
+
+
+async function borrarProducto(id) {
+    const result = await connection.query('DELETE FROM productos WHERE id = ?', id);
+    return result[0];
+}
+
+
+module.exports = {
+    traerProductos, traerProducto, actualizarProducto, crearProducto, borrarProducto
+}
