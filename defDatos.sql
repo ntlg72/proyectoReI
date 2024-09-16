0d0e98b41b89de18f7da7256818f108082c608f2
ntlg72 <166937483+ntlg72@users.noreply.github.com>
2024-09-16 15:59:45 -0500

    Add files via upload

---
+++ defDatos.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE usuarios_BD;
+
+USE usuarios_BD;
+
+CREATE TABLE usuarios (
+
+    username VARCHAR(50) PRIMARY KEY,
+    email VARCHAR(100),
+    nombre VARCHAR(50),
+    password VARCHAR(50),
+    customer_city VARCHAR(50),
+    direccion VARCHAR(100),
+    documento_de_identidad VARCHAR(20)
+);
+
+
+CREATE DATABASE productos_BD;
+
+USE productos_BD;
+
+
+CREATE TABLE productos (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_category VARCHAR(50),
+    product_name VARCHAR(100),
+    product_stock INT,
+    unit_price_cop DECIMAL(10,2)
+);
