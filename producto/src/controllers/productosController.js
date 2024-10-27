815551fe59a4ffc956405d32622b7c7020642a6f
Michelburgos <143130589+Michelburgos@users.noreply.github.com>
2024-10-26 20:34:21 -0500

    nn

--- producto/src/controllers/productosController.js
+++ producto/src/controllers/productosController.js
@@ -56,20 +56,12 @@ router.post('/productos', async (req, res) => {
 
 router.get('/productos/nombre/:product_name', async (req, res) => {
     const { product_name } = req.params;
-
-    try {
-        const productos = await traerProductosPorNombre(product_name);
-        
-        if (productos.length > 0) {
-            return res.status(200).json(productos); // Devuelve los productos encontrados
-        } else {
-            return res.status(404).json({ message: 'No se encontraron productos.' }); // Maneja el caso cuando no hay productos
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Error en el servidor.' }); // Manejo de errores
-    }
+    var result;
+    result = await productosModel.traerProductosPorNombre(product_name) ;
+    //console.log(result);
+    res.json(result[0]);
 });
+
 
 router.put('/productos/:product_id', async (req, res) => {
     const product_id = req.params.product_id;
